28fadddf93e7cf07e42416accae5d6924255f1b7
kanjogoat99 <bluedarkblade54@gmail.com>
2026-02-16 19:53:09 -0500

    Prime home placeholder

---
+++ App/Componets/componet.tsx
@@ -0,0 +1,12 @@
+export default function PrimeHome() {
+  return (
+    <div className="min-h-screen flex items-center justify-center text-white">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold">Prime</h1>
+        <p className="mt-2 text-neutral-400">
+          Watch. Play. Create. Earn Shards.
+        </p>
+      </div>
+    </div>
+  );
+}
