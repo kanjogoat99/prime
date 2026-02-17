0abbfdb7f02562789728b57e33f9b3aaf4e8c5dd
kanjogoat99 <bluedarkblade54@gmail.com>
2026-02-16 19:49:23 -0500

    Update tailwind.config.ts

--- App/Componets/tailwind.config.ts
+++ App/Componets/tailwind.config.ts
@@ -1,1 +1,14 @@
+import type { Config } from "tailwindcss";
 
+const config: Config = {
+  content: [
+    "./app/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}"
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
+
+export default config;
